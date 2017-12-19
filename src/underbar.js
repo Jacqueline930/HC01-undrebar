16cc7b6d5af7da90876d613b27ad26b401593d98
Eduardo Barranon <barranoneduardo@gmail.com>
2017-12-19 10:57:19 -0600

    Part 1 last element problem

--- src/underbar.js
+++ src/underbar.js
@@ -38,6 +38,10 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    return n === undefined ? array[array.length -1] : array.slice(array.length -n);
+    if(n > array.length){
+      return array;
+    }
   };
 
   // Call iterator(value, key, collection) for each element of collection.
