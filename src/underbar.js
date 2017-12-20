95f991495715fc1c304c8a87a86e8e815fed5ab4
Eduardo Barranon <barranoneduardo@gmail.com>
2017-12-20 16:40:30 -0600

    worked on the reject function

--- src/underbar.js
+++ src/underbar.js
@@ -80,10 +80,25 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var result = [];
+    _.each(collection, function(item){
+      if(test(item)){
+        result.push(item);
+      }
+    });
+    return result;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
+    var result = [];
+      _.filter(collection, function(item){
+        if(!test(item)){
+          result.push(item);
+        }
+      });
+      return result;
+
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
   };
